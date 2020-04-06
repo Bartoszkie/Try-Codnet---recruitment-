61bf040dfb0c2ff43b66064d6f9272e6d3a27515
Bartoszkie <bartek.slysz23@gmail.com>
2020-04-06 22:32:19 +0200

    conecting banner-info component to redux

--- codnet-recruitment/src/components/banner-info/banner-info.component.jsx
+++ codnet-recruitment/src/components/banner-info/banner-info.component.jsx
@@ -6,27 +6,29 @@ import Typography from "../typography/typography.component";
 
 const BannerInfo = (props) => {
 
-  
+  if (props.cryptocurrency.loading) {
+    return <p>Is Loading</p>;
+  }
 
   return (
     <BannerInfoContainer>
       <BannerInfoBox>
         <Typography type="p">Market Cap</Typography>
-        <Typography type="h4">$ 117,193,515,285</Typography>
-        <Typography type="p">16,973,125 BTC</Typography>
+        <Typography type="h4">{props.cryptocurrency.cryptocurrency.market_data.market_cap.usd} USD</Typography>
+        <Typography type="p">{props.cryptocurrency.cryptocurrency.market_data.market_cap.btc} BTC</Typography>
       </BannerInfoBox>
       <BannerInfoBox>
         <Typography type="p">Volume (24h)</Typography>
-        <Typography type="h4">$ 7,446,440,000</Typography>
-        <Typography type="p">956,733 BTC</Typography>
+        <Typography type="h4">{props.cryptocurrency.cryptocurrency.market_data.high_24h.usd}</Typography>
+        <Typography type="p">{props.cryptocurrency.cryptocurrency.market_data.high_24h.btc}BTC</Typography>
       </BannerInfoBox>
       <BannerInfoBox>
-        <Typography type="p">Circulating Supplyp</Typography>
-        <Typography type="h4">16,973,125 BTC</Typography>
+        <Typography type="p">circulating_supply</Typography>
+        <Typography type="h4">{props.cryptocurrency.cryptocurrency.market_data.circulating_supply}</Typography>
       </BannerInfoBox>
       <BannerInfoBox>
         <Typography type="p">Max Supply</Typography>
-        <Typography type="h4">21,000,000 BTC</Typography>
+        <Typography type="h4">{props.cryptocurrency.cryptocurrency.market_data.total_supply}</Typography>
       </BannerInfoBox>
     </BannerInfoContainer>
   );
