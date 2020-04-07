c854f41d1ad6796b5a595fdc5428180b52392c78
Bartoszkie <bartosz.slysz23@gmail.com>
2020-04-07 20:47:43 +0200

    sample chart included

--- codnet-recruitment/src/components/chart/chart.component.jsx
+++ codnet-recruitment/src/components/chart/chart.component.jsx
@@ -10,42 +10,13 @@ class Chart extends React.Component {
     super(props);
     this.state = {
       type: "line",
-      data: this.generateChartData(),
+      data: generateChartData(),
       chartedProps: [
         { prop: "sales", lineColor: "#49a6f4" },
         { prop: "commission", lineColor: "#000000" },
       ],
     };
   }
-
-  generateChartData = () => {
-    const { market_data } = this.props.currency.cryptocurrency;
-    const { cryptocurrency } = this.props.currency;
-
-    var chartData = [];
-    var numPoints = 1000;
-    // current date
-    var firstDate = moment();
-    // now set 500 minutes back
-    firstDate.subtract(numPoints, "day");
-
-    // and generate 500 data items
-    var sales = market_data.market_cap.usd;
-    var commission = cryptocurrency.market_data.current_price.usd;
-    for (var i = 0; i < numPoints; i++) {
-      var newDate = moment(firstDate);
-      // each time we add one minute
-      newDate.add(i, "day");
-      chartData.push({
-        date: newDate.format("YYYY-MM-DD"),
-        sales,
-        sales_locale: sales.toLocaleString("en-GB"),
-        commission,
-        commission_locale: Number(commission).toLocaleString("en-GB"),
-      });
-    }
-    return chartData;
-  };
 
   generateChartData2 = () => {
     const { market_data } = this.props.currency.cryptocurrency;
