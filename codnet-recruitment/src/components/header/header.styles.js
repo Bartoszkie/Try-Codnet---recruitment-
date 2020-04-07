1185dcc506775745adde52f095b0a7c43104d57f
Bartoszkie <bartek.slysz23@gmail.com>
2020-04-07 08:18:27 +0200

    added proper styling to header

--- codnet-recruitment/src/components/header/header.styles.js
+++ codnet-recruitment/src/components/header/header.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const HeaderContainer = styled.header`
   grid-column: full-start / full-end;
@@ -9,44 +9,59 @@ export const HeaderContainer = styled.header`
   justify-content: space-between;
   align-items: center;
 
-  box-shadow: 0 4px 2px -2px rgba(0, 0, 0, .1);
+  box-shadow: 0 4px 2px -2px rgba(0, 0, 0, 0.1);
 
   padding: 2rem 2rem;
+  position: relative;
 `;
 
 export const HeaderButton = styled(Link)`
-    font-size: 1.6rem;
-    text-decoration: none;
+  font-size: 1.4rem;
+  text-decoration: none;
+  color: ${({ theme }) => theme.greyTextColor};
+  transition: 0.2s ease-in-out all;
 
-    margin: 0 .9rem;
+  margin: 0 2.5rem;
+
+  &:hover {
+    color: ${({ theme }) => theme.darkBlueTextColor};
+    text-decoration: underline;
+  }
 `;
 
 export const HeaderNavigation = styled.div`
-  justify-self: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 export const HeaderSwitch = styled.select`
-
+  box-shadow: 0px 5px 10px #a3abba33;
+  border-radius: 5px;
+  padding: 0.6rem 1rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
 `;
 
-export const HederSwitchOption = styled.option`
+export const HederSwitchOption = styled.option``;
 
-`;
-
-export const HeaderLogo = styled.img`
-
-`;
+export const HeaderLogo = styled.img``;
 
 export const HeaderAppName = styled.div`
   margin-left: 1rem;
+  font-size: 2.2rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.lightBlueTextColor};
 `;
 
 export const HeaderAppNameBold = styled.span`
-
+  color: ${({ theme }) => theme.darkBlueTextColor};
 `;
 
 export const HeaderButtonLogo = styled(Link)`
-  display: flex; 
+  display: flex;
   justify-content: center;
   align-items: center;
 
