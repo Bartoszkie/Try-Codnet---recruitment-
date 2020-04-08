69aa7c5b574fa93bc1c6ad7575b94e6f8d3c42bf
Bartoszkie <bartosz.slysz23@gmail.com>
2020-04-08 10:53:38 +0200

    changed color opacity to modal

--- codnet-recruitment/src/components/modal/modal.styles.js
+++ codnet-recruitment/src/components/modal/modal.styles.js
@@ -6,7 +6,7 @@ export const ModalContainer = styled.div`
   left: 0%;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.5);
   z-index: 2000;
 
   display: flex;
